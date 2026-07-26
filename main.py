b0645bb3436026cd705bfbdb8ee99611f62861ee
ambsprad <ambsprad@gmail.com>
2026-07-26 11:48:47 -0400

    Add income tracking feature

--- main.py
+++ main.py
@@ -9,7 +9,42 @@ def display_menu():
     print("5. Exit")
 
 
+def add_income(transactions):
+
+    while True:
+        have_income = input("Do you have income to add? Y for Yes N for No. ").strip().upper()
+
+        if have_income == "Y":
+            new_transaction = {
+                'type': "income",
+                'amount': float(input("Enter income amount: ")),
+                'date': input("Enter date of income (MM-DD-YYYY): "),
+                'description': input("Enter description of income: ")
+                }
+            transactions.append(new_transaction)
+
+        elif have_income == "N":
+            if transactions:
+                print("You added the following income transactions: ")
+                for transaction in transactions:
+                    print(
+                        f"\nIncome Amount: ${transaction["amount"]:.2f}"
+                        f"\nDate Recevied: {transaction["date"]}" 
+                        f"\nDescription: {transaction["description"]}"
+                    )         
+
+            else:
+                print("You did not add any income transactions.")
+            print("Returning to main menu.")
+            break
+
+        else:
+            print("You entered an invalid response. Please enter Y or N for your response.")
+
+
+
 def main():
+    transactions = []
     while True:
         display_menu()
 
@@ -17,6 +52,7 @@ def main():
 
         if choice == "1":
             print("\nAdd Income Selected")
+            add_income(transactions)
         
         elif choice == "2":
             print("\nAdd Expense Selected")
